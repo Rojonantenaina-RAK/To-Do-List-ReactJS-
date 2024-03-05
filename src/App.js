22c9d82f7a629e1adf97082947ff2a5df2365bee
Rojonantenaina-RAK <rojo.rak39@gmail.com>
2024-03-06 02:27:32 +0300

    🗒 Mise en place du Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../node_modules/bulma/css/bulma.min.css';
+import Header from './Components/Header/Header.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <div className="container px-3">
+        <h2 className="title mt-5">Rentrez vos tâches à faire</h2>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <nav className='navbar has-text-centered has-background-primary'>
+      <h2 className="is-size-4 has-text-white" style={{lineHeight: '52px'}}>App To Do List</h2>
+    </nav>
+  )
+}
+
+export default Header;
